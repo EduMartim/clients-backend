c7f1ac88dcde03417ea92ffcce1ab789ab43a61e
edumartim <edumartim@gmail.com>
2024-01-28 16:10:12 -0300

    adjusting pom version and User.class

--- clients/src/main/java/com/edu/models/User.java
+++ clients/src/main/java/com/edu/models/User.java
@@ -1,62 +1,64 @@
 package com.edu.models;
 
-import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-
+import java.util.Objects;
 
 @Entity
-@Table(name = User.TABLE_NAME)
+@Table (name = User.TABLE_NAME)
 public class User {
-    public Interface CreateUser{
 
-    }
-
-    public Interface UpdateUser{
-
-    }
+    public interface CreateUser{}
+    public interface UpdateUser{}
 
     public static final String TABLE_NAME = "user";
 
-    @Id
+    /******  ID  ******/
+    @Id 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", unique = true)
     private Long id;
 
-    @Column(name = "username", length = 100, nullable = true)
-    @NotNull(groups = { CreateUser.class, UpdateUser.class })
-    @NotEmpty(groups = { CreateUser.class, UpdateUser.class })
-    @Size(groups = { CreateUser.class, UpdateUser.class }, min = 2, max = 100)
+    /******  User Name  ******/
+    @Column(name = "username", length = 100, nullable = false, unique = true)
+    @NotNull(groups = CreateUser.class)
+    @NotEmpty(groups = CreateUser.class)
+    @Size(groups = CreateUser.class, min = 2, max = 100)
     private String username;
 
+    /******  Password  ******/
     @JsonProperty(access = Access.WRITE_ONLY)
     @Column(name = "password", length = 60, nullable = false)
-    @NotNull
-    @NotEmpty
-    @Size(min = 8, max = 60)
+    @NotNull(groups = { CreateUser.class, UpdateUser.class })
+    @NotEmpty(groups = { CreateUser.class, UpdateUser.class })
+    @Size(groups = { CreateUser.class, UpdateUser.class }, min = 8, max = 60)
     private String password;
 
-    // private List<Task> tasks = new ArrayList<Task>();
+    //private List<Task> tasks = new ArrayList<Task>{};
+
 
     public User() {
     }
+
 
     public User(Long id, String username, String password) {
         this.id = id;
         this.username = username;
         this.password = password;
     }
+
+
 
     public Long getId() {
         return this.id;
@@ -81,6 +83,7 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
 
     @Override
     public boolean equals(Object obj) {
@@ -115,82 +118,4 @@ public class User {
 
 }
 
-/*
- Here is a code generated as suggestion bu ChatGPT 
 
-@Entity
-@Table(name = User.TABLE_NAME)
-public class User {
-
-    private static final String TABLE_NAME = "user";
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", unique = true)
-    private Long id;
-
-    @Column(name = "username", length = 100, nullable = true)
-    @NotNull(groups = { ValidationGroups.CreateUser.class, ValidationGroups.UpdateUser.class })
-    @NotEmpty(groups = { ValidationGroups.CreateUser.class, ValidationGroups.UpdateUser.class })
-    @Size(groups = { ValidationGroups.CreateUser.class, ValidationGroups.UpdateUser.class }, min = 2, max = 100)
-    private String username;
-
-    @Column(name = "password", length = 60, nullable = false)
-    @NotNull
-    @NotEmpty
-    @Size(min = 8, max = 60)
-    private String password;
-
-    // Constructors, getters, setters, and other methods...
-    public User() {
-    }
-
-
-    public User(Long id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-    }
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return this.username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-
-        User user = (User) obj;
-        return Objects.equals(id, user.id) &&
-                Objects.equals(username, user.username) &&
-                Objects.equals(password, user.password);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-}
-*/
