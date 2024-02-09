33fcecc3f6cbb5901bb2ecfab8a43c0b20c3c727
edumartim <edumartim@gmail.com>
2024-02-09 11:59:43 -0300

    implementing WebConfig for cors

---
+++ clients/src/main/java/com/edu/configs/WebConfig.java
@@ -0,0 +1,15 @@
+package com.edu.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
